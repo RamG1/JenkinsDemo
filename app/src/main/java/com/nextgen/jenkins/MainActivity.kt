320bb8408f9d1fdfbe4e1cf3a48be6296572c325
ram <sendmail2rambabu@gmail.com>
2024-08-01 14:49:48 +0530

    This is first Commit2

--- app/src/main/java/com/nextgen/jenkins/MainActivity.kt
+++ app/src/main/java/com/nextgen/jenkins/MainActivity.kt
@@ -15,6 +15,7 @@ import com.nextgen.jenkins.ui.theme.JenkinsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ////////////////////////
         setContent {
             JenkinsTheme {
                 // A surface container using the 'background' color from the theme
