f5a01dce4ed5ad0a68cc7ec2b5f9517326004df5
ram <sendmail2rambabu@gmail.com>
2024-09-19 13:18:30 +0530

    This is first Commit

--- app/src/main/java/com/nextgen/jenkins/MainActivity.kt
+++ app/src/main/java/com/nextgen/jenkins/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android11")
+                    Greeting("Android Jenkins base")
                 }
             }
         }
